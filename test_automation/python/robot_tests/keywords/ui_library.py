2a63ffd9fb0aab8c60b75bc8f736eca17bd9d11b
Mark Hintermeister <Markhint22@yahoo.com>
2026-03-20 16:27:21 -0500

    format update

--- test_automation/python/robot_tests/keywords/ui_library.py
+++ test_automation/python/robot_tests/keywords/ui_library.py
@@ -20,7 +20,7 @@ class UILibrary:
     def open_browser_to_login(self):
         """Open browser to login"""
         self.playwright = sync_playwright().start()
-        self.browser =  self.playwright.chromium.launch(headless=False)
+        self.browser = self.playwright.chromium.launch(headless=False)
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
         self.login_page = LoginPage(self.page)
